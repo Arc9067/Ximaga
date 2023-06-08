691b2b1c4d653f7c33f7e1059afaadf649400363
Arc <atandarokib@gmail.com>
2023-06-08 21:01:27 +0100

    up

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-[20px] w-full">
+      <div className="container flex justify-center items-center flex-col gap-5">
+        <div className="w-full h-1 border-t"></div>
+
+        <p className="font-secondary capitalize font-bold">
+          © 2023 zimanga All Right Reserved{" "}
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
